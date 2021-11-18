f3a6dc0080ab664ae5e72ffb7f0e667e1929bf76
Hritwik Tripathi <tripathi.hritwik@gmail.com>
2021-11-19 01:07:43 +0530

    new start

---
+++ vv/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <button>Start</button>
+      <button>Create Private Lobby</button>
+      <input id="lobby" type="text" />
+    </div>
+  );
+}
+
+export default App;
---
+++ vv/src/views/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function main() {
+    return (
+        <div>
+            <h1>heeheheh main page </h1>
+        </div>
+    )
+}
